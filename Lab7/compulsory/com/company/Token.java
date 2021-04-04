07e122373d5a3e188d22b9fb4487759f50dd344f
valeria-izvoreanu <79272451+valeria-izvoreanu@users.noreply.github.com>
2021-04-04 16:59:23 +0300

    Update Token.java

--- Lab7/compulsory/com/company/Token.java
+++ Lab7/compulsory/com/company/Token.java
@@ -10,6 +10,7 @@ public class Token implements Comparable<Token> {
     }
 
     Token(int n) {
+        //choose random token
         Random random = new Random();
         this.first = random.nextInt(n) + 1;
         int second = random.nextInt(n) + 1;
@@ -29,6 +30,7 @@ public class Token implements Comparable<Token> {
 
     @Override
     public int compareTo(Token other) {
+        //compare Tokens according to it's numbers
         if (other == null) throw new NullPointerException();
         if ((this.second == other.getSecond()) && (this.first == other.getFirst())) return 0;
         else return 1;
