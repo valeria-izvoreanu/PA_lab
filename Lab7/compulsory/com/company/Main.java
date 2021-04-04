778e305e3f9f55255ebb2a626ae19e8531671f82
valeria-izvoreanu <79272451+valeria-izvoreanu@users.noreply.github.com>
2021-04-04 16:56:13 +0300

    Update Main.java

--- Lab7/compulsory/com/company/Main.java
+++ Lab7/compulsory/com/company/Main.java
@@ -3,8 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //create game as in buffer
         Game game = new Game();
+        //create Board as in Producer
         new Board(4, game).start();
+        //create Players as in Consumers
         for (int i = 0; i < 4; i++) {
             new Player("player" + i, game).start();
         }
